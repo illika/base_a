61b086c350587ed6269a79fb006ec093a6e85ec8
illika <steve.milan7@gmail.com>
2023-01-24 19:28:13 -0500

    Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'intro';
+  title: string = 'Contador App';
+
+  numero: number = 0;
+  base : number = 10;
+
+  aumentar(): void {
+    this.numero += 1;
+  }
+
+  disminuir(): void {
+    this.numero -= 1;
+  }
+
+  acumular(valor: number): void {
+    this.numero += valor;
+  }
 }
